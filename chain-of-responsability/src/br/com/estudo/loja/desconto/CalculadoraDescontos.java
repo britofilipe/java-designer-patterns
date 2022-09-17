506ac6d9149520d2a8da7f5e633156cb8d0ec1de
C1317931 Filipe Brito Correia <c1317931@interno.bb.com.br>
2022-09-17 08:28:45 -0300

    Adicionado classe calculadora de desconto

--- chain-of-responsability/src/br/com/estudo/loja/desconto/CalculadoraDescontos.java
+++ chain-of-responsability/src/br/com/estudo/loja/desconto/CalculadoraDescontos.java
@@ -2,9 +2,9 @@ package br.com.estudo.loja.desconto;
 
 import java.math.BigDecimal;
 
-import br.com.estudo.loja.Orcamento;
+import br.com.estudo.loja.orcamento.Orcamento;
 
-public class CalculadoraDeDescontos {
+public class CalculadoraDescontos {
 	
 	public BigDecimal calcular(Orcamento orcamento) {
 		Desconto desconto = new RegraQuantidadeItens(
@@ -13,4 +13,18 @@ public class CalculadoraDeDescontos {
 		return desconto.calcular(orcamento);
 	}
 
+	public BigDecimal calcularAntigo(Orcamento orcamento) {
+
+		if (orcamento.getQuantidadeItens() > 5) {
+			return orcamento.getValor().multiply(new BigDecimal("0.1"));
+		}
+
+		if (orcamento.getValor().compareTo(new BigDecimal("500")) > 0) {
+			return orcamento.getValor().multiply(new BigDecimal("0.1"));
+		}
+
+		return BigDecimal.ZERO;
+	}
+
 }
+
